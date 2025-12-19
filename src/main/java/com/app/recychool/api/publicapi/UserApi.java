08d8f24d374237bd45c1c68cda5a31c02e21ed2f
realdevchani <devchan0208@gmail.com>
2025-12-19 16:13:29 +0900

    UserAPI 수정

--- src/main/java/com/app/recychool/api/publicapi/UserApi.java
+++ src/main/java/com/app/recychool/api/publicapi/UserApi.java
@@ -16,7 +16,7 @@ import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/user/*")
+@RequestMapping("/users/*")
 public class UserApi {
 
     private final PasswordEncoder passwordEncoder;
